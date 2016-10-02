c85b5c39594b2ba9b8dbaa2e39cb54430731e0e0
clouder69 <patrik.suchovsky@gmail.com>
2016-10-02 23:11:32 +0200

    v0.2

--- crun.sh
+++ crun.sh
@@ -6,18 +6,23 @@
 # $1 - name of *.c script
 #
 
+echo " "
+
 file="$1"			#get file name
 if [ -f "$file.c" ] 
 then				#exist file?
 
 	if gcc -o $1 $1.c; 	#compilation success
 	then
-		echo "Compilation was successful. Starting program $file:"
+		echo "# Compilation was successful. Starting program $file:"
+		echo " "
 		./$file		#run program
 	else
-		echo "Compilation was not successful. Program $file cannot be started"
+		echo "# Compilation was not successful. Program $file cannot be started"
 	fi
 
 else				#file not exists
-	echo "File $file.c not found. Program can't be compiled."
+	echo "# File $file.c not found. Program can't be compiled."
 fi
+
+echo " "
